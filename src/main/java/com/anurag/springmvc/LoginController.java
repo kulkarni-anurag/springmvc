53838942e6c2b38b03353806018f236ab6214699
Anurag Kulkarni <kanurag0801@gmail.com>
2022-05-11 11:21:04 +0530

    Getting a param from post request

--- src/main/java/com/anurag/springmvc/LoginController.java
+++ src/main/java/com/anurag/springmvc/LoginController.java
@@ -3,6 +3,7 @@ package com.anurag.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -13,7 +14,8 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String handleLoginPostRequest(){
+    public String handleLoginPostRequest(@RequestParam String name){
+        System.out.println(name);
         return "welcome";
     }
 }
