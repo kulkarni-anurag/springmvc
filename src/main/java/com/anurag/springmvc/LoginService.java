d1a792e6df9d676ef8ea4985b3ce164f05bafeac
Anurag Kulkarni <kanurag0801@gmail.com>
2022-05-13 22:56:03 +0530

    Using spring's autowiring

--- src/main/java/com/anurag/springmvc/LoginService.java
+++ src/main/java/com/anurag/springmvc/LoginService.java
@@ -1,5 +1,8 @@
 package com.anurag.springmvc;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
     public boolean validateUser(String username, String password){
         if(username.equals("virat") && password.equals("virat123")){
