0847b645079117f5fca55baf53f75487d48b9df5
Anurag Kulkarni <kanurag0801@gmail.com>
2022-05-14 23:12:17 +0530

    Added a post request handler for add todo

--- src/main/java/com/anurag/springmvc/todo/TodoController.java
+++ src/main/java/com/anurag/springmvc/todo/TodoController.java
@@ -1,10 +1,13 @@
 package com.anurag.springmvc.todo;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -23,5 +26,11 @@ public class TodoController {
     @RequestMapping(value = "/addtodo", method = RequestMethod.GET)
     public String addTodoPage(){
         return "todo";
+    }
+
+    @RequestMapping(value = "/addtodo", method = RequestMethod.POST)
+    public String handleAddTodo(@RequestParam String desc, ModelMap model){
+        service.addTodo("virat", desc, new Date(), false);
+        return "redirect:list-todos";
     }
 }
